60cd3a7bab5878f62413292cabe106902eb12878
Arif <nurarifns18@gmail.com>
2021-10-11 18:45:34 +0800

    update

---
+++ arithmetic.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int r = 10;
+	double pi = 3.1495;
+	double circumference;
+	
+	circumference = 4*pi*r;
+	
+	cout << circumference;
+  return 0;
+}
+
--- session1.cpp
+++ session1.cpp
@@ -3,6 +3,15 @@ using namespace std;
 
 int main() {
 
+	int x = 10;
+	float a = 10.1;
+	double b = 10.9;
+	char k = 'A';
+	string Name = "Arif";
+	bool test = false;	
+
+	cout << "Value: " << a;
+
   return 0;
 }
 
