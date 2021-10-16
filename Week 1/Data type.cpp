9d75d05da82137efa3b35f738cabe0fb8e65e13d
Arif <nurarifns18@gmail.com>
2021-10-16 23:30:54 +0800

    Update comment

---
+++ Week 1/Data type.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+/*
+	Data type:
+	1. int: integer
+	2. float: decimal, single precision
+	3. double: decimal, double precision
+	4. char: character
+	5. string: array of character
+	6. bool: true/false
+*/
+	int x = 10, y;
+	float a = 10.1;
+	double b = 10.9;
+	char k = 'A';
+	string Name = "Arif";
+	bool test = false;	
+
+	y = 3;
+	
+	cout << "Value: " << a << "\n";
+	cout << "Value: " << y << "\n";
+	
+
+  return 0;
+}
+
--- Week 1/arithmetic.cpp
+++ Week 1/arithmetic.cpp
@@ -3,6 +3,18 @@
 using namespace std;
 
 int main() {
+	
+	/*
+	Arithmetic: 
+	1. bracket: ()
+	2. division: /, and multiplication: *, remainder: %
+	3. Addition: + and subtraction: -
+	
+	** %: modulus
+	
+	Will always follows BODMAS rule
+	*/
+	
 	int r = 10;
 	double pi = 3.1495;
 	double circumference;
