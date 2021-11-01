224b04cabb8d83c95d5485b049f5ee337f630197
Arif <nurarifns18@gmail.com>
2021-11-01 17:09:36 +0800

    week 3

---
+++ Week 2/25-10 Code/Test.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main(){
+	int input,power;
+	
+//	cout << "Enter 2 number separated by space" <<endl;
+//	cin >> input >> power;
+//	cout << pow(input, power);
+
+	cout << "Enter a number ";
+	cin >> input;
+	cout << sqrt(input);
+	return 0;
+}
+
+
