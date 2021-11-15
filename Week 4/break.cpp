b3bb7a1d215be58afc6966ee9ad1a50c9163a59d
Arif <nurarifns18@gmail.com>
2021-11-15 19:12:25 +0800

    week 4 (break fix)
    fixed break.cpp file cout incorrect sum values

--- Week 4/break.cpp
+++ Week 4/break.cpp
@@ -9,37 +9,40 @@ int main(){
 	
 	cin >> num;
 	
-//	while (cin && !isNegative)
-//	{
-//	if (num < 0) //if num is negative, terminate the loop
-//	//after this iteration
-//	{
-//	cout << "Negative number found in the data." << endl;
-//	cout << "Sum: "<< sum << endl;
-//	isNegative = true;
-//	}
-//	else
-//	{
-//	sum = sum + num;
-//	cin >> num;
-//	}
-//	}
-	
-	
-	/*withiout variable isNegative*/
-	sum = 0;
-	cin >> num;
-	while (cin)
+	while (cin && !isNegative)
 	{
 	if (num < 0) //if num is negative, terminate the loop
+	//after this iteration
 	{
 	cout << "Negative number found in the data." << endl;
-	cout << "Sum: "<< sum << endl;
-	break;
+	isNegative = true;
 	}
+	else
+	{
 	sum = sum + num;
 	cin >> num;
+	} 
 	}
+	cout << "Sum: "<< sum << endl;
+
+	
+	
+//	/*without variable isNegative*/
+//	sum = 0;
+//	cin >> num;
+//	while (cin)
+//	{
+//	if (num < 0) //if num is negative, terminate the loop
+//	{
+//	cout << "Negative number found in the data." << endl;
+//
+//	break;
+//	}
+//	sum = sum + num;
+//	cin >> num;
+//
+//	}
+//	cout <<"value: " <<sum;
 		return 0;
 }
 
